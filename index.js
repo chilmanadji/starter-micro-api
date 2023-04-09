43327aeae551b060a163ae3aab4c0af3f6dee7af
chilmanadji <chilman.adji@gmail.com>
2023-04-09 21:37:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var http = require('http');
 http.createServer(function (req, res) {
+    const Http = new XMLHttpRequest();
+    const url='https://jsonplaceholder.typicode.com/posts';
+    Http.open("GET", url);
+    Http.send();
+    
     console.log(`Just got a request at ${req.url}!`)
     res.write(req.url);
     res.end();
