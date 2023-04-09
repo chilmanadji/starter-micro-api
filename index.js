537cb91dd6298ce43cac0780aa3fe0500a87cfc9
chilmanadji <chilman.adji@gmail.com>
2023-04-09 21:43:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
-var http = require('http');
+const http =  require("http")
 
-var options = {
-    host: 'google.com',
-    path: '/'
-}
-var request = http.request(options, function (res) {
-    var data = '';
-    res.on('data', function (chunk) {
-        data += chunk;
-    });
-    res.on('end', function () {
-        console.log(data);
+const server = http.createServer( (req, res) => {
+    res.end("Hello, World!")
+})
 
-    });
-});
-request.on('error', function (e) {
-    console.log(e.message);
-});
-request.end();
+const PORT = process.env.PORT || 3000
+
+server.listen(PORT, console.log(`listening on PORT ${PORT}`))
